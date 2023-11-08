82f1669c0863319a5baebc84a3013016b0a498d7
johnloane <john.loane@dkit.ie>
2023-11-08 17:27:57 +0000

    Adding Pubnub so Pi can just run sensors
    Pi now just deals with sensors and published to Pubnub channel
    when motion is detected

--- PIRBuzzer/sensors.py
+++ PIRBuzzer/sensors.py
@@ -1,35 +1,132 @@
+#from flask import Flask, render_template
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+import os
+from dotenv import load_dotenv
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory, PNOperationType
+import json
 import RPi.GPIO as GPIO
-import time
+import time, threading
+
+load_dotenv()
+
+pnconfig = PNConfiguration()
+
+pnconfig.subscribe_key = os.environ.get('PUBNUB_SUBSCRIBE_KEY')
+pnconfig.publish_key = os.environ.get('PUBNUB_PUBLISH_KEY')
+pnconfig.user_id = "john_sd3b_dkit"
+pubnub = PubNub(pnconfig)
+
+my_channel = "johns_sd3b_pi"
+
+def my_publish_callback(envelope, status):
+    #Check whether request successfully completed or not
+    if not status.is_error():
+        pass
+    else:
+        pass
+
+
+class MySubscribeCallback(SubscribeCallback):
+    def presense(self, pubnub, presence):
+        pass
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            #pubnub.publish().channel(my_channel).message("Hello world").pn_async(my_publish_callback)
+            pass
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+
+    def message(self, pubnub, message):
+        print(message.message)
+        #received = json.loads(message.message)
+        received = message.message
+        if isinstance(received, str):
+            received = json.loads(received)
+        print(type(received))
+        if isinstance(received, dict) and "buzzer" in received.keys():
+            if received["buzzer"] == "on":
+                data["alarm"] = True
+            else:
+                data["alarm"] = False
+
+pubnub.add_listener(MySubscribeCallback())
+
+def publish(channel, message):
+    pubnub.publish().channel(channel).message(message).pn_async(my_publish_callback)
+
+
+#app = Flask(__name__)
+alive = 0
+data = {}
 
 PIR_pin = 23
-BUZZER_pin = 24
+Buzzer_pin = 24
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_pin, GPIO.IN)
-GPIO.setup(BUZZER_pin, GPIO.OUT)
-
-def main():
-    motion_detection()
+GPIO.setup(Buzzer_pin, GPIO.OUT)
 
 def beep(repeat):
     for i in range(0, repeat):
         for pulse in range(60):
-            GPIO.output(BUZZER_pin, True)
+            GPIO.output(Buzzer_pin, True)
             time.sleep(0.001)
-            GPIO.output(BUZZER_pin, False)
+            GPIO.output(Buzzer_pin, False)
             time.sleep(0.001)
         time.sleep(0.02)
 
 
+
 def motion_detection():
+    data["alarm"] = False
+    trigger = False
     while True:
         if GPIO.input(PIR_pin):
             print("Motion detected")
             beep(4)
+            publish(my_channel, {"motion":"Yes"})
+            trigger = True
+        elif trigger:
+            publish(my_channel, {"motion":"No"})
+            trigger = False
+        if data["alarm"]:
+            beep(2)
         time.sleep(1)
 
 
-if __name__ == '__main__':
-    main()
+#@app.route("/")
+#def index():
+#    return render_template("index.html")
 
+
+#@app.route("/keep_alive")
+#def keep_alive():
+#    global alive, data
+#    alive += 1
+#    keep_alive_count = str(alive)
+#    data['keep_alive'] = keep_alive_count
+#    parsed_json = json.dumps(data)
+#    return str(parsed_json)
+
+
+#@app.route("/status=<name>-<action>", methods=["POST"])
+#def event(name, action):
+#    global data
+#    if name == "buzzer":
+#        if action == "on":
+#            data["alarm"] = True
+#        elif action == "off":
+#            data["alarm"] = False
+#    return str("Ok")
+
+
+if __name__ == '__main__':
+    sensorsThread = threading.Thread(target=motion_detection)
+    sensorsThread.start()
+    pubnub.subscribe().channels(my_channel).execute()
